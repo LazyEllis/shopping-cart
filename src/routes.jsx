38e45520c0207e0108a372347a8eeb79f122bee1
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-05-04 02:29:09 +0100

    Add page for purchasing products

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import Layout from "./components/Layout";
 import App from "./routes/App";
 import Store from "./routes/Store";
+import Product from "./routes/Product";
 
 const routes = [
   {
@@ -9,6 +10,7 @@ const routes = [
     children: [
       { index: true, element: <App /> },
       { path: "store", element: <Store /> },
+      { path: "store/:productId", element: <Product /> },
     ],
   },
 ];
---
+++ src/routes/Product.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { useOutletContext, useParams } from "react-router-dom";
+import { CircleMinus, CirclePlus } from "lucide-react";
+import { formatProductText, toTitleCase, formatCurrency } from "../utils/utils";
+import styles from "../styles/Product.module.css";
+
+const Product = () => {
+  const [quantity, setQuantity] = useState(1);
+  const { products } = useOutletContext();
+  const { productId } = useParams();
+
+  const increaseQuantity = () => setQuantity(quantity + 1);
+
+  const decreaseQuantity = () => quantity > 1 && setQuantity(quantity - 1);
+
+  const product = products.find(
+    (product) => product.id === parseInt(productId)
+  );
+
+  return (
+    <>
+      <div className={styles.header}>
+        <h1>Buy {formatProductText(product.title)}</h1>
+        <div>From {formatCurrency(product.price)}</div>
+        <div>{toTitleCase(product.category)}</div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <img src={product.image} alt="" className={styles.productImage} />
+        </div>
+        <div className={styles.productText}>
+          <div className={styles.productInfo}>
+            <div className={styles.productTitle}>
+              {formatProductText(product.title)}
+            </div>
+            <div>{formatCurrency(quantity * product.price)}</div>
+            <div>{toTitleCase(product.category)}</div>
+            <div className={styles.quantitySetter}>
+              <button
+                className={styles.quantityToggle}
+                onClick={decreaseQuantity}
+              >
+                <CircleMinus className={styles.buttonIcon} size={18} />
+              </button>
+              <div>{quantity}</div>
+              <button
+                className={styles.quantityToggle}
+                onClick={increaseQuantity}
+              >
+                <CirclePlus className={styles.buttonIcon} size={18} />
+              </button>
+            </div>
+          </div>
+          <p>{product.description}</p>
+          <button className={styles.addToCartBtn}>Add to Bag</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
