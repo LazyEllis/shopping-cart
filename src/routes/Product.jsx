30c9ddb3a722fe266e79e0613b172433718cfe13
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-05-12 02:19:43 +0100

    Remove unnecessary price controls in product page
    The price controls have been removed from the product page as handling
    of pricing is more suitable to be dealt with in the shopping bag page.

--- src/routes/Product.jsx
+++ src/routes/Product.jsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
 import { useOutletContext, useParams } from "react-router-dom";
-import { CircleMinus, CirclePlus } from "lucide-react";
 import { formatProductText, toTitleCase, formatCurrency } from "../utils/utils";
 import styles from "../styles/Product.module.css";
 
 const Product = () => {
-  const [quantity, setQuantity] = useState(1);
   const { products, cart, setCart } = useOutletContext();
   const { productId } = useParams();
-
-  const increaseQuantity = () => setQuantity(quantity + 1);
-
-  const decreaseQuantity = () => quantity > 1 && setQuantity(quantity - 1);
 
   const handleAddToCart = () => {
     const isInCart = cart.some((product) => product.id === parseInt(productId));
@@ -48,23 +41,8 @@ const Product = () => {
             <div className={styles.productTitle}>
               {formatProductText(product.title)}
             </div>
-            <div>{formatCurrency(quantity * product.price)}</div>
+            <div>{formatCurrency(product.price)}</div>
             <div>{toTitleCase(product.category)}</div>
-            <div className={styles.quantitySetter}>
-              <button
-                className={styles.quantityToggle}
-                onClick={decreaseQuantity}
-              >
-                <CircleMinus className={styles.buttonIcon} size={18} />
-              </button>
-              <div>{quantity}</div>
-              <button
-                className={styles.quantityToggle}
-                onClick={increaseQuantity}
-              >
-                <CirclePlus className={styles.buttonIcon} size={18} />
-              </button>
-            </div>
           </div>
           <p>{product.description}</p>
           <button className={styles.addToCartBtn} onClick={handleAddToCart}>
